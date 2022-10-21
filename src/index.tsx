babe30d6c7388fbcaf14d6bcc6c792b698342262
dofedo <signingx@gmail.com>
2022-10-21 17:02:02 -0400

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import { StrictMode } from 'react';
+import  { createRoot } from 'react-dom/client';
+import TextEditor from './components/text-editor';
+
+
+const App = () => {
+  return (
+    <div>
+      <TextEditor />
+    </div>
+  );
+};
+
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
